441dadc6e8e2a943503734284c810f09f853011e
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2023-08-18 01:22:23 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,62 @@
+"use strict"
+const result = document.querySelector('#resultado')
+
+const nan = document.querySelector('')
+
+
+function inserir(valor) {
+
+    const limite = document.querySelector('#resultado').innerText;
+
+    if (['+', '-', '*', '/'].includes(limite.slice(-1)) && ['+', '-', '*', '/'].includes(valor)) {
+
+        return;
+    }
+
+
+     document.querySelector('#resultado').innerText += valor
+  
+       
+  
+    }
+
+    
+   
+
+function clean () {
+    resultado.innerHTML = ''
+    calculator.innerHTML = ''
+}
+
+
+
+function backspace () {
+    if (resultado.textContent) {
+        const result = document.getElementById('resultado').innerHTML
+
+        resultado.innerHTML = result.substring(0, result.length -1)
+
+
+
+
+    }
+}
+
+
+
+const igual = () => {
+    if(resultado.textContent != 'Erro') {
+        document.querySelector('#calculator').innerHTML = resultado.innerHTML
+
+
+        document.getElementById('resultado').innerHTML = eval(resultado.innerHTML)
+    
+    
+}
+}
+
+
+if (result == NaN || Infinity)  {
+    document.getElementById("resultado").innerHTML = "Erro, não é possivel dividir por 0!"
+}
+
